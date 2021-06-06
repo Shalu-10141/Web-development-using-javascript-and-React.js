df64be1096a54c24ad757b331a951ab0c7f766c6
SHALINI M.Z <mmzeribbabel@gmail.com>
2021-06-06 15:23:20 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() 
+{
+  return 
+  {
+    <div>
+      <h1>javascript and React.js</h1>
+      <p>
+        This was an amazingbootcamp taken up by sharya Sir.
+        We had a great experience
+       </p>
+     </div>
+  };
+}
+export default Note;
